e55381e56fe492915eb244722bc37e8184fd57e6
Sin93 <zinovev.maxim93@mail.ru>
2021-02-07 20:00:08 +0300

    добавлены контроллеры и шаблоны корзины

--- basketapp/views.py
+++ basketapp/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect, get_object_or_404
+from basketapp.models import Basket
+from mainapp.models import Product
 
-# Create your views here.
+
+def basket(request):
+    basket_items = Basket.objects.filter(user=request.user)
+
+    content = {
+        'title': 'корзина',
+        'basket_items': basket_items,
+    }
+
+    return render(request, 'basketapp/basket.html', content)
+
+
+def basket_add(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+
+    basket = Basket.objects.filter(user=request.user, product=product).first()
+
+    if not basket:
+        basket = Basket(user=request.user, product=product)
+
+    basket.quantity += 1
+    basket.save()
+
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+
+def basket_remove(request, pk):
+    basket_record = get_object_or_404(Basket, pk=pk)
+    basket_record.delete()
+
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
