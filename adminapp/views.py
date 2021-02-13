fd3f9b37fc93424624c29bccffc92e82601c96ab
Sin93 <zinovev.maxim93@mail.ru>
2021-02-13 18:55:56 +0300

    create adminapp

---
+++ adminapp/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render
+
+
+def user_create(request):
+    pass
+
+def users(request):
+    pass
+
+def user_update(request):
+    pass
+
+def user_delete(request):
+    pass
+
+def category_create(request):
+    pass
+
+def categories(request):
+    pass
+
+def category_update(request):
+    pass
+
+def category_delete(request):
+    pass
+
+def product_create(request):
+    pass
+
+def products(request):
+    pass
+
+def product_read(request):
+    pass
+
+def product_update(request):
+    pass
+
+def product_delete(request):
+    pass
